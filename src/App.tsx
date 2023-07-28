053981fe1e68b2da73e5f21ee79b85f657cf2735
eupedrorodrigues <pedrorsrodrigues1203@gmail.com>
2023-07-28 15:59:26 -0300

    Fixed login screen conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ScreenLogin from './pages/Login/Login'
-import Login from './pages/Login/login'
 import  Register from './pages/Register/Register'
 import  FormRegister from './pages/FormRegister/FormRegister'
 
 function App() {
   return (
     <div className="App">
-      {/*<ScreenLogin /> */}
-      {/* <Login /> */}
+      <ScreenLogin />
       {/* <Register /> */}
-      <FormRegister/>
+      {/* <FormRegister/> */}
     </div>
   );
 }
