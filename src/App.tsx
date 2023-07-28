8e06dc7ad77710d6d7f08287a6a19c289479daf4
ariad <ariadnederezende@hotmail.com>
2023-07-28 16:39:16 -0300

    create structure edit profile

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import './App.css';
 import Login from './pages/Login/login'
 import  Register from './pages/Register/Register'
 import  FormRegister from './pages/FormRegister/FormRegister'
+import  EditProfile from './pages/EditProfile/EditProfile'
 
 function App() {
   return (
     <div className="App">
       {/* <Login /> */}
       {/* <Register /> */}
-      <FormRegister/>
+      {/* <FormRegister/> */}
+      <EditProfile/>
     </div>
   );
 }
