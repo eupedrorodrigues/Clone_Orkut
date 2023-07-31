67835f357410ab34f3902f31dd44c11b0d485446
GLopes <gicaalopes2003@gmail.com>
2023-07-30 22:33:05 -0300

    adding navbar to edit-profile page

--- src/pages/EditProfile/EditProfile.tsx
+++ src/pages/EditProfile/EditProfile.tsx
@@ -8,17 +8,22 @@ type Props = {};
 
 const EditProfile = (props: Props) => {
   return (
-    <div className={styles.formParagraph}>
-      <div className={styles.wrapperFormFull}>
-        <div className={styles.divImg}>
-          <span className={styles.textHover}>Editar</span>
-          <img src={Profile} alt="name_image" width={120}/>
-        </div>
-        <div className={styles.wrapperFormBtn}>
-          <FormEditProfile />
-          <button className={styles.btnEdit}>Editar perfil</button>
+    <>
+      <Navbar />
+      <h1>Edite seu Perfil</h1>
+      <div className={styles.formParagraph}>
+        <div className={styles.wrapperFormFull}>
+          <div className={styles.divImg}>
+            <span className={styles.textHover}>Editar</span>
+            <img src={Profile} alt="name_image" width={120} />
+          </div>
+          <div className={styles.wrapperFormBtn}>
+            <FormEditProfile />
+            <button className={styles.btnEdit}>Editar perfil</button>
+          </div>
         </div>
       </div>
-    </div>
-  )
-}
+    </>
+  );
+};
+export default EditProfile;
